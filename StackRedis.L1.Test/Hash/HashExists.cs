a3f576ad30bf8192ccceac9fcecaa9a21146d18f
Jonathan Cardy <jcardy@gmail.com>
2016-01-05 16:33:18 +0000

    Tests for HashExists

---
+++ StackRedis.L1.Test/Hash/HashExists.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
+using StackRedis.L1.MemoryCache;
+using System.Threading.Tasks;
+
+namespace StackRedis.L1.Test
+{
+    [TestClass]
+    public class HashExists : UnitTestBase
+    {
+        [TestMethod]
+        public void HashExists_KeyPresent_NotInMemory()
+        {
+            _redisDirectDb.HashSet("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(0, CallsByMemDb);
+            
+            Assert.IsTrue(_memDb.HashExists("hashKey", "key1"));
+            Assert.AreEqual(1, CallsByMemDb);
+        }
+
+        [TestMethod]
+        public void HashExists_KeyPresent_InMemory()
+        {
+            _memDb.HashSet("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(1, CallsByMemDb);
+
+            //This is when HashExist is useful - the key is already in memory.
+            Assert.IsTrue(_memDb.HashExists("hashKey", "key1"));
+            Assert.AreEqual(1, CallsByMemDb);
+        }
+
+        [TestMethod]
+        public void HashExists_KeyMissing_NotInMemory()
+        {
+            _redisDirectDb.HashSet("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(0, CallsByMemDb);
+            
+            Assert.IsFalse(_memDb.HashExists("hashKey", "key3"));
+            Assert.AreEqual(1, CallsByMemDb);
+        }
+
+        [TestMethod]
+        public void HashExists_KeyMissing_InMemory()
+        {
+            _memDb.HashSet("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(1, CallsByMemDb);
+            
+            Assert.IsFalse(_memDb.HashExists("hashKey", "key3"));
+            Assert.AreEqual(2, CallsByMemDb);
+        }
+    }
+}
---
+++ StackRedis.L1.Test/Hash/HashExistsAsync.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
+using StackRedis.L1.MemoryCache;
+using System.Threading.Tasks;
+
+namespace StackRedis.L1.Test
+{
+    [TestClass]
+    public class HashExistsAsync : UnitTestBase
+    {
+        [TestMethod]
+        public async Task HashExistsAsync_KeyPresent_NotInMemory()
+        {
+            await _redisDirectDb.HashSetAsync("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(0, CallsByMemDb);
+            
+            Assert.IsTrue(await _memDb.HashExistsAsync("hashKey", "key1"));
+            Assert.AreEqual(1, CallsByMemDb);
+        }
+
+        [TestMethod]
+        public async Task HashExistsAsync_KeyPresent_InMemory()
+        {
+            await _memDb.HashSetAsync("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(1, CallsByMemDb);
+
+            //This is when HashExist is useful - the key is already in memory.
+            Assert.IsTrue(await _memDb.HashExistsAsync("hashKey", "key1"));
+            Assert.AreEqual(1, CallsByMemDb);
+        }
+
+        [TestMethod]
+        public async Task HashExistsAsync_KeyMissing_NotInMemory()
+        {
+            await _redisDirectDb.HashSetAsync("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(0, CallsByMemDb);
+            
+            Assert.IsFalse(await _memDb.HashExistsAsync("hashKey", "key3"));
+            Assert.AreEqual(1, CallsByMemDb);
+        }
+
+        [TestMethod]
+        public async Task HashExistsAsync_KeyMissing_InMemory()
+        {
+            await _memDb.HashSetAsync("hashKey", new HashEntry[] { new HashEntry("key1", "value1"), new HashEntry("key2", "value2") });
+            Assert.AreEqual(1, CallsByMemDb);
+            
+            Assert.IsFalse(await _memDb.HashExistsAsync("hashKey", "key3"));
+            Assert.AreEqual(2, CallsByMemDb);
+        }
+    }
+}
